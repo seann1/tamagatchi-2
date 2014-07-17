03516a3430ea111302d6f3980ffdeb7f96628382
Eric Johnson & Sean Niesen <student@epicodus.com>
2014-07-17 11:38:56 -0700

    add jquery frontend

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 var tamagotchi = {
-    name: "",
-    foodLevel: 0,
-    sleepLevel: 0,
-    activityLevel: 0,
     initialize: function(initializedName) {
       this.name = initializedName;
       this.foodLevel = 10;
       this.sleepLevel = 10;
       this.activityLevel = 10;
     },
+    name: "",
+    foodLevel: 0,
+    sleepLevel: 0,
+    activityLevel: 0,
     timePasses: function() {
       this.foodLevel --;
       this.sleepLevel --;
@@ -46,20 +46,46 @@ var tamagotchi = {
 
 $(document).ready(function() {
   $("form#petCreator").submit(function(event){
+    var newPet = Object.create(tamagotchi);
     var petName = $("input#petName").val();
+    newPet.initialize(petName);
 
+    $("input#petName").val("");
+    $("div#create-tom").hide();
+    $("div#tom").show();
 
-  var newPet = Object.create(tamagotchi);
+    var time = window.setInterval(function(){
+      if (newPet.isAlive() === true) {
+        newPet.timePasses();
+        $("#foodNum").text(newPet.foodLevel);
+        $("#sleepNum").text(newPet.sleepLevel);
+        $("#activityNum").text(newPet.activityLevel);
+        $("#putName").text(petName);
+      } else {
+        window.clearInterval(time);
+        $("#tom").hide();
+        $("#alert").text(petName + " Is Dead");
+      }
+    }, 1000);
 
-  var time = window.setInterval(function(){
-    if (newPet.isAlive === true) {
-      newPet.timePasses();
-    } else {
-      window.clearInterval(time);
-    }
-  }, 1000);
+    $("#putName").text(petName);
+    $("button#feed").click(function(event){
+      newPet.foodLevel = 10;
+      $("#foodNum").text(newPet.foodLevel);
+      event.preventDefault();
+    });
+     $("button#sleep").click(function(event){
+      newPet.sleepLevel = 10;
+      $("#sleepNum").text(newPet.sleepLevel);
+      event.preventDefault();
+    });
+     $("button#play").click(function(event){
+      newPet.activityLevel = 10;
+      $("#activityNum").text(newPet.activityLevel);
+      event.preventDefault();
+    });
 
-  event.preventDefault();
+    event.preventDefault();
   });
 });
 
